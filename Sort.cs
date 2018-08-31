cfa307af4523ef0e81e6f0922da4c988e6bab6fd
Luciffer007 <zerro0770@gmail.com>
2018-08-31 17:29:08 +0300

    Begining project (Bubble Sort)

---
+++ Sort.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SortLibrary
+{
+    public static class Sort
+    {
+
+        public static double[] BubbleSort(double[] array)
+        {
+            // A comparison cycle of the i-th element with subsequent elements of the array.
+            for (int i = 0; i < array.Length - 1; i++)
+            {
+                for (int j = i + 1; j < array.Length; j++)
+                {
+                    // If the i-th element of the array is greater than the j-th element...
+                    if (array[i] > array[j])
+                    {
+                        double temp;
+
+                        // swap them.
+                        temp = array[i];
+                        array[i] = array[j];
+                        array[j] = temp;
+                    }
+
+                }
+            }
+            return array;
+        }
+
+    }
+}
