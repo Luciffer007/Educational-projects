529e723d291c5059aea1c5be0926f4a52e87c601
Luciffer007 <zerro0770@gmail.com>
2018-09-02 18:13:57 +0300

    Add Quick Sort.

--- Sort.cs
+++ Sort.cs
@@ -6,7 +6,7 @@ using System.Threading.Tasks;
 
 namespace SortLibrary
 {
-    public static class Sort
+    public static class SortingAlgorithms
     {
 
         public static double[] BubbleSort(double[] array)
@@ -19,16 +19,50 @@ namespace SortLibrary
                     // If the i-th element of the array is greater than the j-th element...
                     if (array[i] > array[j])
                     {
-                        double temp;
-
                         // swap them.
-                        temp = array[i];
+                        double temp = array[i];
                         array[i] = array[j];
                         array[j] = temp;
                     }
 
                 }
             }
+            return array;
+        }
+
+        public static double[] QuickSort(double[] array, int firstElement, int lastElement)
+        {
+            // Selecting a supporting element.
+            double supportingElemnt = array[(lastElement - firstElement) / 2 + firstElement];
+            // Assignment to variable indices of the beginning and end of a sortable array (subarray)
+            int i = firstElement;
+            int j = lastElement;
+
+            while (i <= j)
+            {
+                // The search for an array element is less than the supporting element.
+                while (array[i] < supportingElemnt && i <= lastElement) i++;
+
+                // The search for an array element is greater than the supporting element.
+                while (array[j] > supportingElemnt && j >= firstElement) j--;
+
+                // Swap elements.
+                if (i <= j)
+                {
+                    double temp = array[i];
+                    array[i] = array[j];
+                    array[j] = temp;
+                    i++;
+                    j--;
+
+                }
+
+            }
+
+            // Calling the sort method for subarrays.
+            if (i < lastElement) QuickSort(array, i, lastElement);
+            if (j > firstElement) QuickSort(array, firstElement, j);
+
             return array;
         }
 
