0b48803e962c86c25ccde3eda76d07d42286d8e9
Luciffer007 <zerro0770@gmail.com>
2018-09-04 15:26:28 +0300

    Innovations: selection of sorting direction for bubble sorting, verification of the correctness of the parameters introduced to the method

--- Sort.cs
+++ Sort.cs
@@ -8,23 +8,40 @@ namespace SortLibrary
 {
     public static class SortingAlgorithms
     {
+        public enum SortingDirection {Asc = 1, Desc = 2};
 
-        public static double[] BubbleSort(double[] array)
+        public static double[] BubbleSort(double[] array, SortingDirection direction)
         {
             // A comparison cycle of the i-th element with subsequent elements of the array.
             for (int i = 0; i < array.Length - 1; i++)
             {
                 for (int j = i + 1; j < array.Length; j++)
                 {
-                    // If the i-th element of the array is greater than the j-th element...
-                    if (array[i] > array[j])
+                    // Sort the elements of the array in ascending or descending order depending on the selected sorting direction.
+                    switch ((int)direction)
                     {
-                        // swap them.
-                        double temp = array[i];
-                        array[i] = array[j];
-                        array[j] = temp;
-                    }
+                        case 1:
+                            // If the i-th element of the array is greater than the j-th element.
+                            if (array[i] > array[j])
+                            {
+                                // Swap elements which passed order condition.
+                                double temp = array[i];
+                                array[i] = array[j];
+                                array[j] = temp;
+                            }
+                            break;
 
+                        case 2:
+                            // If the i-th element of the array is less than the j-th element.
+                            if (array[i] < array[j])
+                            {
+                                // Swap elements which passed order condition.
+                                double temp = array[i];
+                                array[i] = array[j];
+                                array[j] = temp;
+                            }
+                            break;
+                    }
                 }
             }
             return array;
@@ -38,6 +55,13 @@ namespace SortLibrary
             int i = firstElement;
             int j = lastElement;
 
+            // Checking the correctness of the entered data.
+            if (firstElement < 0 || firstElement > array.Length || lastElement < 0 || lastElement > array.Length)
+            {
+                Console.WriteLine("Incorrect data entered. Sorting is not possible.");
+                return array;
+            }
+
             while (i <= j)
             {
                 // The search for an array element is less than the supporting element.
@@ -46,7 +70,7 @@ namespace SortLibrary
                 // The search for an array element is greater than the supporting element.
                 while (array[j] > supportingElemnt && j >= firstElement) j--;
 
-                // Swap elements.
+                // Swap the element less than the supporting element and the element is greater than the supporting element.
                 if (i <= j)
                 {
                     double temp = array[i];
